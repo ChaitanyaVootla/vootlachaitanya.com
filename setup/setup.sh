144ed9f873ca3420dccc012686aecf9e7ca4bc41
Chaitanya Vootla <speedblaze@gmail.com>
2022-11-05 14:57:49 +0530

    test

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,7 +1,7 @@
 yes | sudo apt update
 yes | sudo apt install nginx
 yes | sudo ufw allow 'Nginx HTTP'
-yes | sudo ufw allow 3000
+yes | sudo ufw allow 22
 yes | sudo ufw allow https
 yes | sudo ufw enable
 
@@ -12,4 +12,4 @@ yes | apt-get install python3-certbot-nginx
 
 # enable cert bot for vootlachaitanya.com and www.vootlachaitanya.com
 # !add A records for both the above domains point them to the machine 
-yes | sudo certbot --nginx -d vootlachaitanya.com -d www.vootlachaitanya.com
+sudo certbot --nginx -d vootlachaitanya.com -d www.vootlachaitanya.com
